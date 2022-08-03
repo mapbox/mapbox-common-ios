274513cfdd79b10a11f0bd0c45f71083fd45770d
Zach Toogood <zach.toogood@mapbox.com>
2022-08-03 17:57:49 +0300

    [carthage] Update Carthage to 0.38.0 and use --use-xcframeworks

--- Tests/Integration/test_carthage.sh
+++ Tests/Integration/test_carthage.sh
@@ -6,7 +6,7 @@ ROOT_DIR="${DIR}/../.."
 pushd "${ROOT_DIR}/Tests/Integration/Carthage"
 
 xcodegen generate
-carthage update --platform iOS --use-netrc
+carthage update --platform iOS --use-netrc --use-xcframeworks
 xcodebuild -project CarthageTest.xcodeproj -scheme CarthageTest -destination 'platform=iOS Simulator,name=iPhone 11,OS=latest' build
 
 popd
