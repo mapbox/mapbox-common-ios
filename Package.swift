5b8385898971469931d5d87d8e66dcef81919ba7
Release SDK bot for Core SDK team <core-sdk-team@mapbox.com>
2022-08-29 10:45:58 +0300

    [mapbox-sdk-common] add config for 23.0.0-rc.2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import Foundation
 
 let registry = SDKRegistry()
 let version = "23.0.0-rc.2"
-let checksum = "811752249942f3c62a2474533c68c6812c1c2a01160f509273585d61da1bc0be"
+let checksum = ""
 
 let package = Package(
     name: "MapboxCommon",
