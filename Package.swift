ac885d5d0b9a0a813d4497e0a5ce02624f1e2b34
Barbara Flindt <barbara.flindt@mapbox.com>
2023-01-25 16:28:16 +0200

    [release] Bump to v23.2.2

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "23.3.0-rc.1"
-let checksum = "b00293068da38a51ea958daa0cf050fb703d17e7bd2517031efed646cf3f69b9"
+let version = "23.2.2"
+let checksum = "f02f866897a48466665025194fdfed3bd3452de44d78208adcb5d324dd2be717"
 
 let package = Package(
     name: "MapboxCommon",
