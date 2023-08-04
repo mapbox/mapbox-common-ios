d61d27a56c17c43b3b4813bb573eba3a9e7f622c
Barbara Flindt <barbara.flindt@mapbox.com>
2023-08-04 16:56:24 +0300

    Release-v23.8.0-beta.1

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "24.0.0-beta.2"
-let checksum = "6185eaadeb94d32b64f19e2435aae58c320292fc4b7645239495f5def963d3a9"
+let version = "23.8.0-beta.1"
+let checksum = "742ce98d45b82351299075ceda3df522a566c4b8ce33811d9a91c4e37ac1d958"
 
 let package = Package(
     name: "MapboxCommon",
