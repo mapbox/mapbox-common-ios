04f881a690a9c6ed1ae993a9cb4b4ae42d8f42e1
Sergey Akentev <sergey.akentev@mapbox.com>
2024-05-30 14:00:46 +0300

    Downgrade SPM tools to 5.3

--- scripts/release/Package.swift
+++ scripts/release/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .executableTarget(
+        .target(
             name: "set-marketing-version",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
