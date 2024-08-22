e230e4960e57a5e7de8176ab46091b131412873f
Oleg Fedorov <oleg.fedorov@mapbox.com>
2024-08-22 10:39:06 +0300

    Do a cocoapods retry directly on circleci Instead of triggering circleci retry from github actions

--- Tests/Integration/test_cocoapods.sh
+++ Tests/Integration/test_cocoapods.sh
@@ -5,10 +5,25 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="${DIR}/../.."
 pushd "${ROOT_DIR}/Tests/Integration/CocoaPods"
 
-xcodegen generate
-bundle install
-bundle exec pod repo update
-bundle exec pod install
-xcodebuild -workspace PodInstall.xcworkspace -scheme PodInstall -destination 'platform=iOS Simulator,name=iPhone 14,OS=latest' build CODE_SIGNING_ALLOWED=NO
+echo Publishing to Cocoapods takes a long time so try to build and wait for up to 2 hours
+
+retries=40
+
+for ((i=0; i<retries; i++)); do
+    xcodegen generate &&
+    bundle install &&
+    bundle exec pod repo update &&
+    bundle exec pod install &&
+    xcodebuild -workspace PodInstall.xcworkspace -scheme PodInstall -destination 'platform=iOS Simulator,name=iPhone 14,OS=latest' build CODE_SIGNING_ALLOWED=NO
+
+    [[ $? -eq 0 ]] && break
+
+    echo "something went wrong, let's wait 3 minutes and retry"
+    sleep 180
+done
 
 popd
+
+(( retries == i )) && { echo 'Failed!'; exit 1; }
+exit 0
+
