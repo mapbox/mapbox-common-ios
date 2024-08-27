2752f352650748f4941bb8f1179bb4847b81f91d
Release SDK bot for Core SDK team <core-sdk-team@mapbox.com>
2024-08-27 14:22:10 +0000

    Release v24.7.0-daily-2024-08-27-13-00

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "24.7.0-daily-2024-08-27-09-34"
-let checksum = "c35f6c4a3882daf457e593e7742e59c6a925aeabe1bfcedfadbd1ee5080f70c8"
+let version = "24.7.0-daily-2024-08-27-13-00"
+let checksum = "75634f6ae1f5a46417cda50f93d6f73888f8c69bd3ff345bf39e8ac616c2209d"
 
 let package = Package(
     name: "MapboxCommon",
