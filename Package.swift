a279fe7970d9ad968cdbcf14a505187cd95d6960
Aleksei Sapitskii <aleksei.sapitskii@mapbox.com>
2024-10-23 11:58:42 +0300

    Integrate Turf dependency

--- Package.swift
+++ Package.swift
@@ -4,27 +4,40 @@
 import PackageDescription
 import Foundation
 
-let version = "24.9.0-daily-2024-10-11-04-26"
-let checksum = "787a7d772d16b60c49f8370159e39f13dc23d7910ea40f322e6f31dda43107f7"
+let commonVersion = "24.9.0-daily-2024-10-16-04-26"
+let commonChecksum = "bd443c4c695dfacbc82731e25ba50bbc086afa6c7967043f134f994c8b758c82"
+
+let turfVersion: Version = "3.1.0"
+let turfChecksum = "ccc9517866a15f123377e6554953a369e791a1b2cf6ad13ea11f0123dab38be8"
 
 let package = Package(
     name: "MapboxCommon",
     platforms: [.iOS(.v12), .macOS(.v10_15), .custom("visionos", versionString: "1.0")],
     products: [
-        .library(
-            name: "MapboxCommon",
-            targets: ["MapboxCommon"]
-        )
+        .library(name: "MapboxCommon", targets: ["MapboxCommonWrapper"]),
+        .library(name: "TurfBinary", targets: ["TurfBinary"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/mapbox/turf-swift.git", exact: turfVersion)
     ],
     targets: [
+        .target(name: "MapboxCommonWrapper", dependencies: [
+            .target(name: "TurfBinary"),
+            .target(name: "MapboxCommon")
+        ]),
+        /// Revert to download from storage
         .binaryTarget(
             name: "MapboxCommon",
-            url: "https://api.mapbox.com/downloads/v2/mapbox-common/releases/ios/packages/\(version)/MapboxCommon.zip",
-            checksum: checksum
+            path: "MapboxCommon.xcframework"
         ),
         .testTarget(
             name: "MapboxCommonTests",
-            dependencies: ["MapboxCommon"]
+            dependencies: ["MapboxCommonWrapper"]
+        ),
+        .binaryTarget(
+            name: "TurfBinary",
+            url: "https://github.com/mapbox/turf-swift/releases/download/v\(turfVersion)/Turf.xcframework.zip",
+            checksum: turfChecksum
         )
     ],
     cxxLanguageStandard: .cxx17
--- README.md
+++ README.md
@@ -38,13 +38,13 @@ machine api.mapbox.com
 ###### Using SPM Package 📱🖥💻
 
 ```swift
-.package(url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.7.1"),
+.package(url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.7.0"),
 ```
 
 ##### CocoaPods 📱🖥💻
 
 ```ruby
-pod 'MapboxCommon', '24.7.1'
+pod 'MapboxCommon', '24.7.0'
 ```
 
 ##### Carthage 📱
@@ -52,7 +52,7 @@ pod 'MapboxCommon', '24.7.1'
 Add the following code to your Cartfile.
 
 ```bash
-binary "https://api.mapbox.com/downloads/v2/carthage/mapbox-common/MapboxCommon.json" == 24.7.1
+binary "https://api.mapbox.com/downloads/v2/carthage/mapbox-common/MapboxCommon.json" == 24.7.0
 ```
 
 Then run the following command in the Terminal.
