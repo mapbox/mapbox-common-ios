977c2a12e4f899e594a5552f514e681876271c05
Release SDK bot for Mapbox SDK <release-bot@mapbox.com>
2024-11-18 16:20:28 +0000

    Update SPM and CocoaPods configs for 24.9.0-sapial-enhance-daily-flow-0-daily-2024-11-18-15-10 release

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion = "24.9.0-sapial-release-naive-sdks-1-daily-2024-11-14-17-16"
-let commonChecksum = "8fb013181e748c164fa64c13d865504dbef6cedaedd38bb05b13a7d9fa519bf4"
+let commonVersion = "24.9.0-sapial-enhance-daily-flow-0-daily-2024-11-18-15-10"
+let commonChecksum = "41839e72f4e3270e9d77c3ffda991960739f6335edd1f19240e606bec03a203c"
 
 let turfVersion: Version = "4.0.0-beta.1"
 
